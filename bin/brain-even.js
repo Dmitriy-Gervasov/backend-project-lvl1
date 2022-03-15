e57f70e9986810cd1f3da1c5f887b2792a83f24e
Dmitriy-Gervasov <gervasov@list.ru>
2022-03-15 17:27:19 +0300

    5 шаг проекта

---
+++ bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+// eslint-disable-next-line import/no-unresolved
+import checkEven from "../games/gameeven";
+
+checkEven();
---
+++ games/gameeven.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+// eslint-disable-next-line import/no-unresolved
+import { userName, randomnumber } from '../scr/index.js';
+
+const round = 3;
+
+console.log('Answer "YES" if the number is even,otherwisr answer "NO".');
+const checkEven = () => {
+  for (let i = 1; i <= round; i += 1) {
+    const number = randomnumber(1, 100);
+    console.log(`Question: ${number}`);
+
+    const Answer = readlineSync.question('Your answer: ');
+    if (number % 2 === 0) {
+      if (Answer === 'YES') {
+        console.log('Correct!');
+      } else {
+        console.log(`'${Answer} is wrong Answer; (. Correct answer was 'YES'`);
+        console.log(`Let's try again, ${userName}!`);
+        return;
+      }
+    }
+    if (number % 2 !== 0) {
+      if (Answer === 'NO') {
+        console.log('Correct!');
+      } else {
+        console.log(`'${Answer} is wrong Answer; (. Correct answer was 'NO'`);
+        console.log(`Let's try again, ${userName}!`);
+        return;
+      }
+    }
+  }
+  console.log(`Gongratulations, ${userName}!`);
+};
+
+export default checkEven;
