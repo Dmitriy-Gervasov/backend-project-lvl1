34e1771639313f0e4dde0f148ad0bd19752f4006
Dmitriy-Gervasov <gervasov@list.ru>
2022-05-11 12:01:26 +0300

    исправления

--- src/common.js
+++ src/common.js
@@ -1,7 +1,7 @@
 const getRandomInt = (min = 0, max = 1000) => {
-    const random = Math.ceil(Math.random() * max + min);
-  
-    return random;
-  };
+  const random = Math.ceil(Math.random() * max + min);
+
+  return random;
+};
 
 export default getRandomInt;
